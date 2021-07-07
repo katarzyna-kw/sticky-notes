7d149749e4b1a18c59a31f770302d86ea9bac93d
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-07 17:06:10 -0500

    decompose App.js into components

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => (
+    <header className="app-header">
+        <h1 className="app-header__title">
+        Super Sticky Notes
+        </h1>
+        <aside className="app-header__controls">
+        <button className="add-new">+ New Note</button>
+        <input className="search" type="text" placeholder="Type here to search..." />
+        </aside>
+    </header>
+);
+
+export default Header;
---
+++ src/Note.js
@@ -0,0 +1,15 @@
+const Note = () => (
+    <li className="note">
+      <input 
+        className="note__title" 
+        type="text" 
+        placeholder="Title" 
+      />
+      <textarea 
+        className="note__description"
+        placeholder="Description..." />
+      <span className="note__delete">X</span>
+    </li>
+);
+
+export default Note;
