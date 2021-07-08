b2ad4740b5f87d734366d710989f794fc7cae69a
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-08 12:51:35 -0500

    add notes and searchText state to App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import Header from "./Header";
 import NotesList from "./NotesList";
 
 class App extends Component {
-  state = {};
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "eat",
+        description: "reese peanut butter cups",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "sleep",
+        description: "eight hours",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true
+      }
+    ], 
+    searchText: "Search notes"
+  };
   render() {
     return (
       <div>
