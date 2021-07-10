f851ab2f11eda1ad53a44fd791c2726f31edb72f
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-10 07:49:36 -0500

    Updates

---
+++ precache-manifest.4a81710862acc9a1b14e0511835c2d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad08ff9e8950d8807253",
+    "url": "/sticky-notes/static/css/main.4cb82841.chunk.css"
+  },
+  {
+    "revision": "ad08ff9e8950d8807253",
+    "url": "/sticky-notes/static/js/main.0e288c78.chunk.js"
+  },
+  {
+    "revision": "eb4d8215ed7fa83de4b7",
+    "url": "/sticky-notes/static/js/runtime~main.98137e24.js"
+  },
+  {
+    "revision": "063a48b94bf00760f5e5",
+    "url": "/sticky-notes/static/js/2.689a4215.chunk.js"
+  },
+  {
+    "revision": "0f13adc144b712f3fefe7b36902fc4bd",
+    "url": "/sticky-notes/index.html"
+  }
+];
